789f2b635b5ccd9f0713414603eaff1727de9ed7
juny0192 <80000534+juny0192@users.noreply.github.com>
2021-03-11 19:35:16 -0500

    Update todo-localstorage.js

--- todo-localstorage.js
+++ todo-localstorage.js
@@ -3,6 +3,13 @@ const form = document.querySelector("#submit");
 
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
+for (let i = 0; i < savedTodos.length; i++) {
+	if (savedTodos[i].task === clickItem.innerText) {
+		savedTodos[i].completed = clickItem.completed;
+		localStorage.setItem("todos", JSON.stringify(savedTodos));
+	}
+}
+
 form.addEventListener("submit", function (e) {
 	e.preventDefault();
 
@@ -11,26 +18,25 @@ form.addEventListener("submit", function (e) {
 
 	let newToDoValue = document.getElementById("addToDo").value;
 	newToDo.innerText = newToDoValue;
+	newToDo.completed = false;
 
-	// let newCheckBox = document.createElement("input");
-	// newCheckBox.setAttribute("type", "checkbox");
-	// newToDo.appendChild(newCheckBox);
+	let newCheckBox = document.createElement("input");
+	newCheckBox.setAttribute("type", "checkbox");
+	newToDo.appendChild(newCheckBox);
 
 	form.reset();
 
-	savedTodos.push({ task: newToDoValue });
+	savedTodos.push({ task: newToDoValue, completed: false });
 	localStorage.setItem("todos", JSON.stringify(savedTodos));
 });
 
-// toDoList.addEventListener("click", function (e) {
-// 	let clickItem = e.target;
-
-// 	if (clickItem.tagName === "INPUT") {
-// 		e.target.parentElement.classList.add("completed");
-// 		clickItem.completed = true;
-// 	}
-//     if ( )
-//     {
-// 		clickItem.completed = false;
-// 	}
-// });
+toDoList.addEventListener("click", function (e) {
+	let clickItem = e.target;
+	if (!clickItem.completed) {
+		e.target.parentElement.classList.toggle("completed");
+		clickItem.completed = true;
+	} else {
+		e.target.parentElement.classList.toggle("completed");
+		clickItem.completed = false;
+	}
+});
